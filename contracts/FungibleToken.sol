5e95f822512fea4e1615efafcb590dd0393f865b
Caleb Oneyemi <caloneyemi@gmail.com>
2024-07-08 19:16:29 +0100

    added access modifier for sample fungible token

--- contracts/FungibleToken.sol
+++ contracts/FungibleToken.sol
@@ -2,18 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract MyFungibleToken is ERC20 {
+contract MyFungibleToken is ERC20, Ownable {
     //token name && symbol
-    constructor() ERC20('FungibleToken', 'FTKN') {
+    constructor(
+        address initialOwner
+    ) ERC20('FungibleToken', 'FTKN') Ownable(initialOwner) {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
-    function mint(uint _quantity) public {
+    function mint(uint _quantity) public onlyOwner {
         _mint(msg.sender, _quantity);
     }
 
-    function burn(uint _quantity) public {
+    function burn(uint _quantity) public onlyOwner {
         _burn(msg.sender, _quantity);
     }
 }
